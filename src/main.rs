0c7de084c4fbd46fd5209d6cd94c596599718cfa
sikma363 <sikma363@gmail.com>
2025-10-06 13:43:51 +0700

    feat: interget variable data type

--- src/main.rs
+++ src/main.rs
@@ -62,3 +62,37 @@ fn variable_explicit_implicit_type() {
     let num_b: i8 = 10;
     println!("{}", num_b)
 }
+
+/* Integer Type and Range
+
+Bits        Signed Type   Range (iX)                                                Unsigned Type   Range (uX)
+---------------------------------------------------------------------------------------------------------------
+8-bit       i8            -128 to 127                                              u8              0 to 255
+16-bit      i16           -32,768 to 32,767                                        u16             0 to 65,535
+32-bit      i32           -2,147,483,648 to 2,147,483,647                          u32             0 to 4,294,967,295
+64-bit      i64           -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807  u64             0 to 18,446,744,073,709,551,615
+128-bit     i128          -170,141,183,460,469,231,731,687,303,715,884,105,728     u128            0 to 340,282,366,920,938,463,463,374,607,431,768,211,455
+Arch-dep.   isize         same as i32 on 32-bit, i64 on 64-bit                     usize           same as u32 on 32-bit, u64 on 64-bit
+*/
+
+// default interger use "i32"
+// rust support conversion integer type number, like from i8 -> i64
+// but, still aware in rust have a  "integer overflow" like if converting integer type from large to small
+// or if interger data cannot be acomodateed bt the specified data type
+// to convert data type interger use keyword "as"
+
+#[test]
+fn conversion_integer_type() {
+    let a: i8 = 10;
+    println!("{}", a);
+
+    let b: i64 = a as i64;
+    println!("{}", b);
+
+    // here example of "integrer overflow"
+    let c: i64 = 1000000000;
+    println!("{}", c);
+
+    let d: i8 = c as i8; // return 0 value
+    println!("{}", d)
+}
